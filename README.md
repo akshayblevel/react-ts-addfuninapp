58e8f8246423609a611e44fd3ec369d7446c2da1
akshayblevel <akshayblevel@gmail.com>
2021-07-21 18:43:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-g4zbqd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-g4zbqd)
---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Greet() {
+  return <div>Hello Akshay!</div>;
+}
+
+function App() {
+  return (
+    <div>
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
